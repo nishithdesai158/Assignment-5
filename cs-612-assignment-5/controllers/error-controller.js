43cbd2b32229b7e5011f6e30fede00d140cf71bd
nishithdesai158 <102260866+nishithdesai158@users.noreply.github.com>
2022-05-15 04:59:35 -0400

    Delete error-controller.js

--- cs-612-assignment-5/controllers/error-controller.js
+++
@@ -1,9 +0,0 @@
-exports.show404Page = (req, res) => {
-    return res.status(404).render('404',
-        {
-            pageTitle: 'Page Not Found',
-            path: '/404',
-            stud: null
-        }
-    );
-}
